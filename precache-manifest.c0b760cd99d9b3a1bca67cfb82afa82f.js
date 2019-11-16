48db1d20260c5e9dfed025bc1ec096b161673613
domotord90 <jazzmaster@freemail.com>
2019-11-16 18:50:45 +0100

    Updates

---
+++ precache-manifest.c0b760cd99d9b3a1bca67cfb82afa82f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59c8f3b673ed5bf561af378d7bf2baf5",
+    "url": "/react-todos/index.html"
+  },
+  {
+    "revision": "49d73cb483588d87165d",
+    "url": "/react-todos/static/css/main.a9f62c45.chunk.css"
+  },
+  {
+    "revision": "68add4098f080224e62e",
+    "url": "/react-todos/static/js/2.0f96319f.chunk.js"
+  },
+  {
+    "revision": "49d73cb483588d87165d",
+    "url": "/react-todos/static/js/main.0933b2f7.chunk.js"
+  },
+  {
+    "revision": "ab9ea8a31f3c0d7bac35",
+    "url": "/react-todos/static/js/runtime~main.32ae1423.js"
+  }
+]);
